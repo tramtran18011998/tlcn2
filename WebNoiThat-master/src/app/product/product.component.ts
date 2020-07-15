81de58ea27610db141a93d7855aea329c1c4c69f
PhamGiaThinh <32518909+PhamGiaThinh@users.noreply.github.com>
2020-07-15 23:51:41 +0700

    carousel fix, sản phẩm liên quan(chưa), bình luận (chưa)

--- WebNoiThat-master/src/app/product/product.component.ts
+++ WebNoiThat-master/src/app/product/product.component.ts
@@ -237,7 +237,7 @@ export class ProductComponent implements OnInit {
   }
 
   onOptionsSelected(value: number){
-    // console.log("nani");
+    console.log("nani");
     this.intSelect = value;
     console.log(this.intSelect);
     this.getProductList();
